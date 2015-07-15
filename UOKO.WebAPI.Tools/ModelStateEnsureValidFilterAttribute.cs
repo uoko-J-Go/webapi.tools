52dab380ed14a74bdddc7f69c490432f30f93880
$+j <lust4life.jun@gmail.com>
2015-07-15 19:10:51 +0800

    beta

---
+++ UOKO.WebAPI.Tools/ModelStateEnsureValidFilterAttribute.cs
@@ -0,0 +1,49 @@
+﻿using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using System.Web.Http.Controllers;
+using System.Web.Http.Filters;
+
+namespace UOKO.WebAPI.Tools
+{
+    /// <summary>
+    /// 这个是做整个 ModelState 强制校验的.
+    /// 如果 ModelState 有错误, 那么就直接返回统一的错误信息.
+    /// 可以通过指定 OverrideModelStateEnsureFilter 来达到排除,
+    /// 然后自己在方法体中做相应的处理.
+    /// </summary>
+    public class ModelStateEnsureValidFilterAttribute : ActionFilterAttribute
+    {
+        public override void OnActionExecuting(HttpActionContext actionContext)
+        {
+            if (actionContext == null)
+            {
+                return;
+            }
+            var request = actionContext.Request;
+            if (request == null)
+            {
+                return;
+            }
+
+            var modelState = actionContext.ModelState;
+            if (modelState == null
+                || modelState.IsValid)
+            {
+                return;
+            }
+
+            // 返回统一错误信息, 用 ErrorInfo 代替 ModelStateKey(ModelState) ,因为除了 ModelState 以外
+            // 还可能是用户自定义的一些 ErrorInfo ,只不过我们都统一使用 ModelState 的格式进行返回.
+            var httpError = new HttpError(modelState, request.ShouldIncludeErrorDetail());
+            object modelStateInfo;
+            var hasModelStateInfo = httpError.TryGetValue(HttpErrorKeys.ModelStateKey, out modelStateInfo);
+            if (hasModelStateInfo)
+            {
+                httpError.Remove(HttpErrorKeys.ModelStateKey);
+                httpError["ErrorInfo"] = modelStateInfo;
+            }
+            actionContext.Response = request.CreateErrorResponse(HttpStatusCode.BadRequest, httpError);
+        }
+    }
+}
---
+++ UOKO.WebAPI.Tools/UnifyExceptionFilter.cs
@@ -0,0 +1,31 @@
+﻿using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using System.Web.Http.Filters;
+
+namespace UOKO.WebAPI.Tools
+{
+    /// <summary>
+    /// 处理 发生在具体 action 或 controller 中的未处理的异常,这个和默认的全局异常处理还不一样.
+    /// 这一类的异常返回 BadRequest(400) 等错误状态.
+    /// </summary>
+    public class UnifyExceptionFilter : ExceptionFilterAttribute
+    {
+        public override void OnException(HttpActionExecutedContext actionExecutedContext)
+        {
+            //actionExecutedContext.
+
+            //var exception = actionExecutedContext.Exception;
+
+            //var request = actionExecutedContext.Request;
+            //var httpError = new HttpError(exception, request.ShouldIncludeErrorDetail())
+            //                {
+            //                    Message = "请求出错,请查看异常信息"
+            //                };
+
+
+            //var response = request.CreateErrorResponse(HttpStatusCode.BadRequest, httpError);
+            //actionExecutedContext.Response = response;
+        }
+    }
+}
