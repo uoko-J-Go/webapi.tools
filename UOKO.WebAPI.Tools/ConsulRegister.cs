655765922c27684ef5022521a3fb380c44404580
$+j <lust4life.jun@gmail.com>
2016-06-15 15:00:38 +0800

    add consul service register

---
+++ UOKO.WebAPI.Tools/ConsulRegister.cs
@@ -0,0 +1,35 @@
+﻿using Consul;
+
+namespace UOKO.WebAPI.Tools
+{
+    public class ConsulRegister
+    {
+
+        public void RegisteService(string port)
+        {
+            
+            var consulClient = new ConsulClient();
+            consulClient.Agent.ServiceRegister(new AgentServiceRegistration()
+                                               {
+                                                   Name =,
+                                                   Address =,
+                                                   Port =,
+                                                   Checks = new[]
+                                                            {
+                                                                new AgentCheckRegistration()
+                                                                {
+                                                                    TCP = "",
+                                                                    Name = "",
+                                                                    Notes = "",
+                                                                    Interval = ,
+                                                                    Timeout = ,
+                                                                },
+                                                            },
+                                               });
+
+
+        }
+
+        
+    }
+}
